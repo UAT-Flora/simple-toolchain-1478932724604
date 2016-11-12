fef6cf494860e65b5cc0aa98b42301a43dd90337
UAT-Flora <wufengl@cn.ibm.com>
2016-11-12 15:30:11 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,52 @@
-/*eslint-env node*/
+/*eslint-env node, express*/
 
 //------------------------------------------------------------------------------
-// hello world app is based on node.js starter application for Bluemix
+// node.js starter application for Bluemix
 //------------------------------------------------------------------------------
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
-var express = require('express');
+var express = require("express");
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+var cfenv = require("cfenv");
 
 // create a new express server
 var app = express();
-
+	
+//add http&socket module
+var http = require("http").Server(app);	
+var io = require("socket.io")(http);	
+	
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
+
+
+io.on("connection",function(socket){
+	
+	socket.on("String",function(key){	
+		if(key == "w" || key == "s" || key == "a" || key == "d"){	
+			console.log("Received " + key + " from frontend");
+			
+			//************************
+			//fill in a method "io.sockets.emit(param1,param2)" in line 35
+			//The first parameter is the data type "message", the second parameter is the data you want to send
+			io.sockets.emit("message", key);		
+		}
+		else{
+			console.log("Not a direction order");
+		}	
+	});
+			
+});
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
 
-	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+http.listen(appEnv.port, "0.0.0.0", function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url + " " + appEnv.port);
 });
